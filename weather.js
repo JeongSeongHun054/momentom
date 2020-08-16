54aee4dbe2a627520f713a4f2090fd9f72806144
JeongSeongHun054 <hun@DESKTOP-6TDILTO.localdomain>
2020-08-17 02:04:40 +0900

    momentom

---
+++ weather.js
@@ -0,0 +1,58 @@
+const weather = document.querySelector('.js-weather');
+
+const API_KEY = 'ff0fc0e0eea1d39a53e46628beb5ba47';
+const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucess(position) {
+  const latitude = position.coords.latitude; //위도
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log('Cant access geo location');
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //getweather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
